d9f0cc60d9c652b8de60c9bbbdd617c7f271b7b3
Valeriy Torgovanov <Faust_XVI@list.ru>
2022-11-26 16:24:22 +0200

    v 1.2

---
+++ Find First Palindromic String in the Array (Easy)/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        StringBuilder wordsBuilder = new StringBuilder();
+        StringBuilder wordsBuilderReverse = new StringBuilder();
+        for(int i = 0; i < words.length; i++)
+        {
+            wordsBuilder = wordsBuilder.append(words[i]);
+            wordsBuilderReverse = wordsBuilder.reverse();
+            if(words[i].equals(wordsBuilderReverse.toString())){
+                return words[i];
+            }
+            else {
+                wordsBuilder = wordsBuilder.delete(0,wordsBuilder.length());
+                wordsBuilderReverse = wordsBuilderReverse.delete(0,wordsBuilder.length());
+            }
+        }
+        return "";
+    }
+}
